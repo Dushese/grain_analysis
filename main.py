cf92bc5afcb746187b7fce20a57c74acfe01723c
Andrew Potapov <avpotapov_5@miem.hse.ru>
2024-02-11 19:52:41 +0300

    СТандартизируем, аналогничный расчет как и для изображений с плотностью 30

--- main.py
+++ main.py
@@ -51,28 +51,28 @@ def count_angles(approxed_arr):
         for i in range(len(cnt)): # Если две вершины очень близко - берем среднее этих вершин
             next_point = cnt[(i + 1) % len(cnt)]
             amount_merged = 0
-        
-            if np.linalg.norm(prev_point[0] - next_point[0]) < 5 and ang_amount > 3:
+
+            if np.linalg.norm(prev_point[0] - next_point[0]) < 3 and ang_amount > 3:
                 amount_merged += 1
                 prev_point = (prev_point + next_point) / 2
+                prev_point = [[np.round(prev_point[0][0]), np.round(prev_point[0][1])]]
                 ang_amount -= amount_merged
             else:
                 cnt_new.append(prev_point)
                 prev_point = cnt[(i + 1) % len(cnt)]
         cnt = np.array(cnt_new, dtype=np.intc)
-        if np.shape(cnt)[0] > 7:  # не учитываю многоугольники с большим количеством точек, так как эти точки не являются вершинами многоугольника
+        if np.shape(cnt)[0] > 10:  # не учитываю многоугольники с большим количеством точек, так как эти точки не являются вершинами многоугольника
             continue
         # cv.drawContours(img, [cnt], -1, (255, 0, 0), 1, cv.LINE_AA)
 
         points = [point[0] for point in cnt]
         points = np.array(points)
+        # print(points)
         flag = 0
         for i in range(len(points)):
             # print(points[i])
-            if ((points[i][0] >= 0 and points[i][0] <= 0 + 5) or (
-                    points[i][0] >= np.shape(img)[0] - 5 and points[i][0] <= np.shape(img)[0])) \
-                    or ((points[i][1] >= 0 and points[i][1] <= 0 + 5) or (
-                    points[i][1] >= np.shape(img)[1] - 5 and points[i][1] <= np.shape(img)[1])):
+            if ((points[i][0] >= 0 and points[i][0] <= 0 + 5) or (points[i][0] >= np.shape(img)[0] - 5 and points[i][0] <= np.shape(img)[0])) \
+                    or ((points[i][1] >= 0 and points[i][1] <= 0 + 5) or (points[i][1] >= np.shape(img)[1] - 5 and points[i][1] <= np.shape(img)[1])):
                 # print(points[i], np.shape(img)[0], np.shape(img)[1])
                 flag = 1
                 # img_test = np.copy(img)
@@ -86,12 +86,21 @@ def count_angles(approxed_arr):
                 break
         if flag == 0:
             for i in range(len(points)):
+                # img_test = np.copy(img)
                 first_vec = np.array(points[i] - points[(i + 1) % len(points)])
                 second_vec = np.array(points[(i + 2) % len(points)] - points[(i + 1) % len(points)])
-
                 angle = round(angle_between(first_vec, second_vec))
 
                 angles.append(angle)
+                # print(angle)
+                # one = points[i]
+                # two = points[(i + 1) % len(points)]
+                # three = points[(i + 2) % len(points)]
+                # img_test = drow_three_points(img_test, one, two, three)
+                # fig, ax = plt.subplots(figsize=(12, 12))
+                # plt.imshow(img_test)
+                # plt.show()
+
                 # if angle == 135:
                 #     counter += 1
                 #     img = cv.imread(img_name)
@@ -103,6 +112,10 @@ def count_angles(approxed_arr):
             # cv.imshow('135', img)
             # cv.waitKey()
             # cv.destroyAllWindows()
+
+    # cv.imshow('cvcv', img)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
     return angles
 
 
@@ -134,23 +147,29 @@ def show_cluster(clusters_matrix, x_shape, y_shape, cluster):
 def count_area(approxed_arr):
     """
     :param approxed_arr: координаты вершин многоугольников
-    :return: новый массив координат, плотность зерен, средняя площадь зерна
+    :return: новый массив координат, плотность зерен, средняя площадь зерна, массив площадей
     """
     all_area = 0
+    not_real_grain = 0 # для подсчета ненастоящих зерен
     areas = []
     new_approxed_arr = []
     for cnt in approxed_arr:
         area = Polygon([point[0] for point in cnt]).area
         if area < 1: # удаляю случайно выделенные области - очень маленькие
+            not_real_grain += 1
             continue
         new_approxed_arr.append(cnt)
-        areas.append(area)
+        areas.append(round(area/100.0)) # нужно добиться какого нужного округления
         all_area += area
-
-    print(f'Средняя площадь зерна в пикселях = {all_area / len(approxed_arr)}')
+    print(areas)
+    print(f'Средняя площадь зерна в пикселях = {all_area / (len(approxed_arr) - not_real_grain)}')
     print(
-        f' площадь всех зерен = {all_area} \n площадь изображения = {np.shape(img)[0] * np.shape(img)[1]} \n плотность зерен = {all_area / (np.shape(img)[0] * np.shape(img)[1])} \n максимальная площадь зерна = {np.max(areas)} \n кол-во зерен = {len(areas)}')
-    return new_approxed_arr, all_area / (np.shape(img)[0] * np.shape(img)[1]), all_area / len(approxed_arr)
+        f' площадь всех зерен = {all_area} \n '
+        f'площадь изображения = {np.shape(img)[0] * np.shape(img)[1]} \n '
+        f'плотность зерен = {all_area / (np.shape(img)[0] * np.shape(img)[1])} \n '
+        f'максимальная площадь зерна = {np.max(areas)} \n ' # обратите внимание на areas, может быть сильно округлен
+        f'кол-во зерен = {len(areas)}')
+    return new_approxed_arr, all_area / (np.shape(img)[0] * np.shape(img)[1]), all_area / (len(approxed_arr) - not_real_grain), areas
 
 
 def resize_img(img):
@@ -174,49 +193,38 @@ def name_files_50_density():
                     yield folder_name + str(folder_1) + '-' + str(folder_2) + '-3400-0.08-0.03/z' + str(
                         img_ind_1) + '.' + str(img_ind_2) + '.png'
 
-
-# def name_files_30_density():
-#     folder_name = "C:/Users/Dushese/files_for_jupiter/grains_density_30/img-"
-#     for folder_1 in range(10, 80, 10):
-#         for img_ind_1 in [4, 5]:
-#             if img_ind_1 == 4:
-#                 indexes = [x for x in range(2, 10)]
-#             else:
-#                 indexes = [x for x in range(0, 9)]
-#             for img_ind_2 in indexes:
-#                 # Clusterisation
-#                 if folder_1 >= 30:
-#                     prism_num = '2550-0.09'
-#                 else:
-#                     prism_num = '3400-0.08'
-#                 yield folder_name + str(folder_1) + '-' + '49-' + prism_num + '-0.03/z' + str(
-#                     img_ind_1) + '.' + str(img_ind_2) + '.png'
+def count_dist(arr: np.array, distribution):
+    '''
+    Считает распределение величины
+    :param arr: массив величин
+    :return: массив, где каждому значению arr сопоставлено количетсво встретившихся значений в arr
+    '''
+    print(arr)
+    for ind in range(np.shape(arr)[0]):
+        if arr[ind] in distribution:
+            distribution[arr[ind]] += 1
+        else:
+            distribution[arr[ind]] = 1
 
 
-# def name_files_30_density_colored():
-#     folder_name = "C:/Users/Dushese/files_for_jupiter/grains_density_30_colored/img-"
-#     for folder_1 in range(10, 100, 10):
-#         if folder_1 == 90:
-#             for folder_1_2 in range(89, 100, 1):
-#                 for img_ind_1 in [4, 5]:
-#                     if img_ind_1 == 4:
-#                         indexes = [x for x in range(2, 10)]
-#                     else:
-#                         indexes = [x for x in range(0, 9)]
-#                     for img_ind_2 in indexes:
-#                         yield folder_name + str(folder_1_2) + '-' + '49-2550-0.09-0.03/z' + str(
-#                             img_ind_1) + '.' + str(img_ind_2) + '.png'
-#         else:
-#             for img_ind_1 in [4, 5]:
-#                 if img_ind_1 == 4:
-#                     indexes = [x for x in range(2, 10)]
-#                 else:
-#                     indexes = [x for x in range(0, 9)]
-#                 for img_ind_2 in indexes:
-#                     yield folder_name + str(folder_1) + '-' + '49-2550-0.09-0.03/z' + str(
-#                         img_ind_1) + '.' + str(img_ind_2) + '.png'
+def plot_hist(distribution: dict, name_distribution: str):
+    # print(name_distribution, distribution)
+    index = sorted(distribution.keys())
+    values = np.array([distribution[ang] for ang in index]) / all_angle_amount
+    print('\n\n')
+    for key in index:
+        distribution[key] = distribution[key] / all_angle_amount
+        if distribution[key] > 0.06:
+            print(f'угол = {key}, кол-во углов = {distribution[key] / all_angle_amount}')
+    np.save(f'{name_distribution}.npy', distribution)
+    fig, ax = plt.subplots()
+    ax.bar(index, values)
+    ax.set_ylabel('Доля углов')
+    ax.set_xlabel('значение угла, градусы')
+    plt.show()
 
 t = time.time()
+# folder_name = "C:/Users/Dushese/files_for_jupiter/grains_density_30/img-"
 density_sum = 0
 average_grain_area_sum = 0
 all_grains_amount = 0
@@ -224,17 +232,7 @@ all_angle_amount = 0
 amount_images = 0
 angle_dist = {}
 
-
-# Изменить!!! Для 50 плотности 110 кластеров, для 30 плотности 50 кластеров
-
-# density_ = 30
-# clstrs: int
-# if density_ == 50:
-#     clstrs = 110
-#     generator = name_files_50_density
-# elif density_ == 30:
-#     clstrs = 6
-#     generator = name_files_30_density_colored
+area_dist = {}
 
 # цикл по изображениям
 for img_name in name_files_50_density():
@@ -247,30 +245,25 @@ for img_name in name_files_50_density():
         "n_init": "auto",
         "random_state": 4
     }
-    y_pred = KMeans(n_clusters=clstrs, **common_params).fit_predict(img)#cv.medianBlur(image_base,5))
+    y_pred = KMeans(n_clusters=110, **common_params).fit_predict(img)#cv.medianBlur(image_base,5))
 
     clusters_matrix = y_pred.reshape(image_base.shape[0], image_base.shape[1])
-    clusters_matrix += 1
+
+
     result_markers = np.zeros((image_base.shape[0], image_base.shape[1]))
     result_image = np.zeros((image_base.shape[0], image_base.shape[1]))
     kernel_erode = np.ones((5, 5), np.uint8)
-    kernel_dilate = np.ones((5, 5), np.uint8)
-
-    for x in range(1, clstrs + 1):
+    kernel_dilate = np.ones((3, 3), np.uint8)
+    flag = 0
+    for x in range(clstrs):
       amount_markered_in_pix, marker_matrix, cluster_image = show_cluster(clusters_matrix, image_base.shape[0], image_base.shape[1], x)
-      # fig, axis = plt.subplots(2, 2)
-      # axis[0, 0].imshow(clusters_matrix)
-      # axis[1, 0].imshow(marker_matrix)
       if amount_markered_in_pix > image_base.shape[0] * image_base.shape[1] / 4: # если кол-во точек, принадлежащих одному кластеру равно четверти всей картинке и больше, то это фон, он нам не нужен
         continue
       marker_matrix = cv.erode(marker_matrix, kernel_erode, iterations=2)
       result_markers += marker_matrix
 
-      # axis[0, 1].imshow(marker_matrix)
-      # plt.show()
-      cluster_image = cv.erode(cluster_image, kernel_dilate, iterations=2)
+      cluster_image = cv.erode(cluster_image, kernel_erode, iterations=2)
       result_image += cluster_image
-
     # cv.imshow('cvcv', result_image)
     # cv.waitKey()
     # cv.destroyAllWindows()
@@ -285,7 +278,6 @@ for img_name in name_files_50_density():
     sure_fg = np.uint8(result_image)
     unknown = cv.subtract(sure_bg, sure_fg)
 
-
     # watershed
     img_real = cv.imread(img_name)
     result_markers = np.array(result_markers, dtype=np.int32)
@@ -295,16 +287,6 @@ for img_name in name_files_50_density():
 
     img_real[markers == -1] = [255, 0, 0]
 
-    # fig, axis = plt.subplots(2, 2)
-    # axis[0, 0].imshow(result_markers)
-    # axis[0, 1].imshow(image_base)
-    # axis[1, 0].imshow(unknown)
-    # axis[1, 1].imshow(img_real)
-    # plt.show()
-
-    # cv.imshow('cvsscv', img_real)
-    # cv.waitKey()
-    # cv.destroyAllWindows()
 
     # approximation
 
@@ -313,6 +295,9 @@ for img_name in name_files_50_density():
     img[markers == -1] = [255, 0, 0]
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_OTSU)
+    # fig, ax = plt.subplots(figsize=(12, 12))
+    # plt.imshow(img_test)
+    # plt.show()
     img[:] = 0
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
@@ -328,7 +313,7 @@ for img_name in name_files_50_density():
     img_new = cv.imread(img_name)
 
     img = img_new.copy()
-    eps = 0.007
+    eps = 0.01
     approxed_arr = []
     for cnt_arr in new_cntrs:
         epsilon = eps * cv.arcLength(cnt_arr, True)
@@ -356,41 +341,51 @@ for img_name in name_files_50_density():
     #         starting_color = ((starting_color[0] + 50) % 255, (starting_color[1] + 5) % 255, (starting_color[2] + 30) % 255)
 
     # Плотность
-    approxed_arr, density, average_grain_area = count_area(approxed_arr)
+    approxed_arr, density, average_grain_area, areas = count_area(approxed_arr)
+
     density_sum += density
     average_grain_area_sum += average_grain_area
     # Распределение углов
     angles = count_angles(approxed_arr)
     all_angle_amount += len(angles)
-    for ang in angles:
-        if ang in angle_dist:
-            angle_dist[ang] += 1
-        else:
-            angle_dist[ang] = 1
-    if amount_images % 10 == 0:
-        name = img_name.split('/')[-2:]
-        name = (name[0] + name[1]).strip('/').strip('.png')
-        cv.imwrite(f'density_{density_}_colored_{name}_segmented.png', img)
-        cv.imwrite(f'density_{density_}_colored_{name}_not_approxed.png', img_real)
+
+    count_dist(angles, angle_dist)
+    count_dist(areas, area_dist)
+
+
+    # for ang in angles:
+    #     if ang in angle_dist:
+    #         angle_dist[ang] += 1
+    #     else:
+    #         angle_dist[ang] = 1
+
+    if img_ind_2 == 2:
+        name = str(folder_1) + '-' + '49' + '-3400-0.08-0.03-z' + str(
+    img_ind_1) + '.' + str(img_ind_2)
+        cv.imwrite(f'{name}_segmented.png', img)
 
 
 print(f'Общие данные \n\tСредняя плотность зерен = {density_sum / amount_images}\n'
       f'\tСредний размер зерна = {average_grain_area_sum / amount_images}')
 print(f'\tКол-во снимков = {amount_images}')
+print(f'\n Epsilon = {eps}')
 print(f'Общее время выполнения: {time.time() - t}')
 
-index = sorted(angle_dist.keys())
-values = np.array([angle_dist[ang] for ang in index]) / all_angle_amount
+plot_hist(angle_dist, 'angle_distribution')
+plot_hist(area_dist, 'area_distribution')
+# print(len(area_dist))
 
-print('\n\n')
-for key in index:
-    angle_dist[key] = angle_dist[key] / all_angle_amount
-    if angle_dist[key] > 0.06:
-        print(f'угол = {key}, кол-во углов = {angle_dist[key] / all_angle_amount}')
-np.save('angle_distribution.npy', angle_dist)
-fig, ax = plt.subplots()
-ax.bar(index, values)
-ax.set_ylabel('Доля углов')
-ax.set_xlabel('значение угла, градусы')
-plt.show()
-# чекаю что гит работает
+# index = sorted(angle_dist.keys())
+# values = np.array([angle_dist[ang] for ang in index]) / all_angle_amount
+#
+# print('\n\n')
+# for key in index:
+#     angle_dist[key] = angle_dist[key] / all_angle_amount
+#     if angle_dist[key] > 0.06:
+#         print(f'угол = {key}, кол-во углов = {angle_dist[key] / all_angle_amount}')
+# np.save('angle_distribution.npy', angle_dist)
+# fig, ax = plt.subplots()
+# ax.bar(index, values)
+# ax.set_ylabel('Доля углов')
+# ax.set_xlabel('значение угла, градусы')
+# plt.show()
